70af6d652a739095f20ce899255ee2fb3b17b249
wesleyfigueira <wesleyfigueira1993@gmail.com>
2026-03-05 18:55:15 -0300

    o

--- pandas/pand.py
+++ pandas/pand.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
+vendas_df = pd.read_csv('contoso - Vendas - 2017.csv', sep=';', encoding='latin1')
+produtos_df = pd.read_csv('contoso - Cadastro Produtos.csv', sep=';', encoding='latin1')
+loja_df = pd.read_csv('contoso - Lojas.csv', sep=';', encoding='latin1')
+cliente_df = pd.read_csv('contoso - Clientes.csv', sep=';', encoding='latin1')
 
 
 
-vendas_df =pd.read_csv('contoso - Vendas - 2017.csv',sep=';')
 
 
-##print(vendas_df[:5])
+## tirar colunas que não serão usadas
+cliente_df=cliente_df.drop(['Unnamed: 7', 'Unnamed: 8', 'Unnamed: 9'], axis=1)
 
-lista_clientes = vendas_df['ID Cliente']
-
-print(lista_clientes)
+print(cliente_df.head())
